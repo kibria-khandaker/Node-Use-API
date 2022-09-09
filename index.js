d900b30c01484766b0a19958dcad6b5c405ae52e
kibria khandaker <46866083+kibria-khandaker@users.noreply.github.com>
2022-09-09 14:21:57 +0600

    All API Created

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ app.get('/user', (req, res) => {
     res.send('Welcome to my users <b>List </b>')
 })
 
+app.get('/user/random', (req, res) => {
+    res.send('Welcome to my user <b> List/random </b>')
+})
+
+app.get('/user/all', (req, res) => {
+    res.send('Welcome to my user <b> List/all </b> ')
+})
+app.get('/user/save', (req, res) => {
+    res.send('Welcome to my user <b> List/save </b> ')
+})
+app.get('/user/update', (req, res) => {
+    res.send('Welcome to my user <b> List/update </b>')
+})
+app.get('/user/bulk-updat', (req, res) => {
+    res.send('Welcome to my user <b> List/bulk-updat </b>')
+})
+app.get('/user/delete', (req, res) => {
+    res.send('Welcome to my user <b> List/delete </b>')
+})
+
+
 app.listen(PORT, () => {
     console.log(`My Node App run on PORT ${PORT}`)
 })
