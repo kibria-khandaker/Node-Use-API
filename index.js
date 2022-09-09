0e08a8db679fb2ea8af8b7f532696cb6c16d395a
kibria khandaker <46866083+kibria-khandaker@users.noreply.github.com>
2022-09-09 23:30:59 +0600

    edit process.env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const randomFun = require('./middleware/randomFun.js');
 const app = express();
 require("dotenv").config();
-const PORT = 5000;
+const PORT = process.env.PORT||5000;
 
 // my middleware
 // app.use(randomFun);
